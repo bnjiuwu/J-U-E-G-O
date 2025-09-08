73b41381e27d3b9fea91ad3e220e92656435f2f7
bnji_uwu <benja.sms.elmilagro@gmail.com>
2025-09-08 02:09:50 -0300

    lol

---
+++ 2d-metroidvania-lol/Scripts/Player/player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+var input
+@export speed = 100.0
+var gravity = 10
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
