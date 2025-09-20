36d84b0ad4f5204e55537d948cb7c1e3c79134f4
bnjiuwu <benja.sms.elmilagro@gmail.com>
2025-09-20 00:16:03 -0300

    plo

--- 2d-metroidvania-lol/Scenes/Slime scene/slime.gd
+++ 2d-metroidvania-lol/Scenes/Slime scene/slime.gd
@@ -3,35 +3,32 @@ extends CharacterBody2D
 @export var speed: float = 50
 @export var patrol_distance: float = 100
 @export var max_health: int = 3
-@onready var animated_sprite = $AnimatedSprite2D
 
+var direction := -1 # -1 = izquierda, 1 = derecha
+@onready var floor_check: RayCast2D = $floor_check
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-var direction = -1
-
-@onready var ray = $RayCast2D
-
+#==== health system ====
 var health: int
 var start_pos: Vector2
-
-
 func _ready():
 	health = max_health
 	start_pos = global_position
 	add_to_group("enemies")
 
 func _physics_process(delta):
-	velocity.x = direction * speed
-	if not is_on_floor():
-		velocity.y += 900 * delta
-
-	# Detectar borde
-	if not ray.is_colliding():
-		direction *= -1
-		scale.x = -scale.x
-		ray.position.x *= -1
-
+	# movimiento
+	velocity.x = speed * direction
 	move_and_slide()
 
+	# el raycast siempre apunta adelante según la dirección actual
+	floor_check.cast_to.x = 10 * direction
+
+	# si está en el suelo y no hay suelo adelante → cambiar dirección
+	if is_on_floor() and not floor_check.is_colliding():
+		direction *= -1
+		sprite.flip_h = direction == 1
+		
 # 🩸 Recibir daño (llamar cuando una bala colisione)
 func take_damage(amount: int):
 	health -= amount
