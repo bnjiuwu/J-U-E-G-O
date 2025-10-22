739742ec8ad6269acde9d34b5472b0e3029ec8d8
bnji_uwu <benja.sms.elmilagro@gmail.com>
2025-10-22 03:54:13 -0300

    Revert "envola seme bugeo la tontera"
    This reverts commit 9b2e88ff3882fbd12432ced51695343fcfbf9484.

--- 2d-metroidvania-lol/Scenes/Player/player.gd
+++ 2d-metroidvania-lol/Scenes/Player/player.gd
@@ -233,5 +233,5 @@ func take_damage(amount) -> void:
 
 func die() -> void:
 	print("💀 Player ha muerto")
-	print("ola")
+	
 	get_tree().reload_current_scene()#aquí puedes cambiarlo por animación/game over
