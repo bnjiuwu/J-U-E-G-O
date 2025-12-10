fecb4bdb2a0cc86a744f973d11e1fdd9f6bc01dd
thefa <azullagos3@gmail.com>
2025-12-10 16:58:54 -0300

    ahora si

---
+++ 2d-metroidvania-lol_nivel3 - copia/Assets/Scenes/MiniMap/mini_map.gd
@@ -0,0 +1,41 @@
+extends CanvasLayer
+@onready var sub_viewport_container: SubViewportContainer = $UI/MarginContainer/SubViewportContainer
+@onready var sub_viewport: SubViewport = $UI/MarginContainer/SubViewportContainer/SubViewport
+@onready var minimap_camera: Camera2D = $"UI/MarginContainer/SubViewportContainer/SubViewport/Minimap Camera"
+@onready var player_marker: ColorRect = $UI/MarginContainer/SubViewportContainer/SubViewport/PlayerMarker
+
+
+var player_node :Node2D
+
+#
+#func _ready() -> void:
+#	for tilemap in owner.get_node("TileMaps").get_children():
+#		var minimap_tilemap = tilemap.duplicate()
+#		_setup_minimap(minimap_tilemap)
+#		
+#		var used_rect: Rect2i = tilemap.get_used_rect()
+#	pass
+#
+
+func _process(delta: float) -> void:
+	if player_node:
+		minimap_camera.global_position = lerp(
+			minimap_camera.global_position,
+			player_node.global_position,0.2
+		)
+		player_marker.global_position = player_node.global_position
+	pass
+
+func _setup_minimap(minimap_tilemap: TileMapLayer) -> void:
+	sub_viewport.add_child(minimap_tilemap)
+	
+	pass
+
+	
+func _set_minimap_limits(used_rect:Rect2i) -> void:
+	minimap_camera.limit_left = used_rect.position.x * 1
+	minimap_camera.limit_top = used_rect.position.y * 16
+	minimap_camera.limit_right = (used_rect.position.x + used_rect.size.x) * 16
+	minimap_camera.limit_bottom = (used_rect.position.y + used_rect.size.y) * 16
+	
+	pass
